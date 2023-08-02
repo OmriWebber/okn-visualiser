8818f729709c694c57b26d7b65ddea881c4fe2a2
Omri Webber <omriwebber@gmail.com>
2023-08-02 15:43:02 +1200

    remove bootstrap

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import './assets/main.scss'
-import { BootstrapVue} from 'bootstrap-vue'
 
-// Import Bootstrap and BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -12,7 +8,5 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
-
-app.use (BootstrapVue)
 
 app.mount('#app')
