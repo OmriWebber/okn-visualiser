2ca3f93247754ca9c8eac9f8cddf551fb41b8c44
Omri Webber <omriwebber@gmail.com>
2023-08-30 09:20:02 +1200

    fix merge

--- src/views/VisualisationView.vue
+++ src/views/VisualisationView.vue
@@ -1,14 +1,7 @@
 <script>
-<<<<<<< HEAD
 import Graph from '../components/visualisation/Graph.vue'
 import Table from '../components/visualisation/Table.vue'
 import FileUpload from '../components/visualisation/fileUpload.vue'
-=======
-import Graph from '../components/Graph.vue'
-import Table from '../components/Table.vue'
-import Controls from '../components/Controls.vue'
-import FileUpload from '../components/fileUpload.vue'
->>>>>>> 02d6cba520e2ce4fb1adcb8e2792d7d7cb5e4ce6
 
 export default {
   components: {
